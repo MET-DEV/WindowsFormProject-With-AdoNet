e5786445b7bfb4a505ada70873408e3c371f6e46
MET <mustafatpe23@gmail.com>
2021-01-17 20:19:12 +0300

    Add project files.

---
+++ TryForAdoNet/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace TryForAdoNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgwStudent = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgwStudent
+            // 
+            this.dgwStudent.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgwStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwStudent.Location = new System.Drawing.Point(61, 32);
+            this.dgwStudent.Name = "dgwStudent";
+            this.dgwStudent.RowHeadersWidth = 51;
+            this.dgwStudent.RowTemplate.Height = 24;
+            this.dgwStudent.Size = new System.Drawing.Size(936, 279);
+            this.dgwStudent.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 498);
+            this.Controls.Add(this.dgwStudent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwStudent)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgwStudent;
+    }
+}
+
---
+++ TryForAdoNet/StudentDal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryForAdoNet
+{
+    class StudentDal
+    {
+        public List<Student> GetAllStudents()
+        {
+            SqlConnection connection = new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=Student;integrated security=true");
+            if (connection.State==ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            SqlCommand command = new SqlCommand("Select * from Students",connection);
+            SqlDataReader reader = command.ExecuteReader();
+            List<Student> students = new List<Student>();
+            while (reader.Read())
+            {
+                Student student = new Student();
+                student.Id = Convert.ToInt32(reader["Id"]);
+                student.Name = Convert.ToString(reader["Name"]);
+                student.Lastname = Convert.ToString(reader["Lastname"]);
+                student.Point = Convert.ToInt32(reader["Point"]);
+                students.Add(student);
+            }
+            return students;
+        }
+    }
+}
